fa877b40e6eae2d32997135797ace7b48d4493ea
Ying Chu Troy Chen <troy8chen@Yings-MacBook-Pro.local>
2025-03-28 12:21:11 -0700

    Updated Firebase setup

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,15 +1,26 @@
-import { initializeApp, getApps, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-import { getAuth } from 'firebase-admin/auth';
-import { adminConfig } from '../firebase.config';
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
-const apps = getApps();
+// Initialize Firebase Admin SDK
+function initFirebaseAdmin() {
+  const apps = getApps();
 
-if (!apps.length) {
-  initializeApp({
-    credential: cert(adminConfig)
-  });
+  if (!apps.length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // Replace newlines in the private key
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
 }
 
-export const db = getFirestore();
-export const auth = getAuth();
+export const { auth, db } = initFirebaseAdmin();
